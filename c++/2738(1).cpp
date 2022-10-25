28de4c62d736faf3e6c41fff2f4b3dcd6712f748
Dain Jung <dainjungdev@gmail.com>
2022-10-25 17:25:09 +0100

    251022

---
+++ c++/2738(1).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> A(n, vector<int>(m));
+    vector<vector<int>> B(n, vector<int>(m));
+
+    // Matrix A
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> A[i][j];
+        }
+    }
+
+    // Matrix B
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> B[i][j];
+        }
+    }
+
+    // Matrix A + Matrix B
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cout << (A[i][j] + B[i][j]) << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
---
+++ c++/2738.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int A[100][100] = {}, B[100][100] = {};
+
+    // Matrix A
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> A[i][j];
+        }
+    }
+
+    // Matrix B
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> B[i][j];
+        }
+    }
+
+    // Matrix A + Matrix B
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cout << (A[i][j] + B[i][j]) << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
