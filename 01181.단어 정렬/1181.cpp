5f3b8f38752b4bb15be75d32ba071b6c6430723a
Dain Jung <dainjungdev@gmail.com>
2022-12-05 00:13:37 +0000

    edit 1181.cpp

--- 01181.단어 정렬/1181.cpp
+++ 01181.단어 정렬/1181.cpp
@@ -13,8 +13,8 @@ int main() {
     cin >> n;
     vector<string> words(n);
 
-    for (int i = 0; i < n; ++i) {
-        cin >> words[i];
+    for (auto &word : words) {
+        cin >> word;
     }
 
     sort(words.begin(), words.end(), [](auto &left, auto &right) {
