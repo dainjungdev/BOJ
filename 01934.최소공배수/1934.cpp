2a7381f5d119ed3b11ef3e752669a44c1b4bdbda
Dain Jung <dainjungdev@gmail.com>
2022-12-08 09:03:51 +0000

    edit 1934.cpp

--- 01934.최소공배수/1934.cpp
+++ 01934.최소공배수/1934.cpp
@@ -13,6 +13,9 @@ int gcd(int a, int b) {
 int lcm(int a, int b) { return a * b / gcd(a, b); }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin >> t;
     for (int i = 0; i < t; ++i) {
