c8c3e0383f196e4dce160e3e7ecb26a0a2b79657
Dain Jung <dainjungdev@gmail.com>
2022-12-08 09:03:08 +0000

    solve 1934.cpp

---
+++ 01934.최소공배수/1934.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+        return gcd(b, a % b);
+    }
+}
+
+int lcm(int a, int b) { return a * b / gcd(a, b); }
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        int n, m;
+        cin >> n >> m;
+        cout << lcm(n, m) << '\n';
+    }
+    return 0;
+}
