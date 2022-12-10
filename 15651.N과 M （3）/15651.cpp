a96013115312f488d79f94c30a998ba4a0e217f2
Dain Jung <dainjungdev@gmail.com>
2022-12-10 12:30:48 +0000

    solve 15651.cpp

---
+++ 15651.N과 M （3）/15651.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+bool visited[8];
+vector<int> permutation_with_repetition;
+
+void dfs(int k) {
+    if (permutation_with_repetition.size() == m) {
+        for (auto &num : permutation_with_repetition) {
+            cout << num << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        permutation_with_repetition.push_back(i);
+        dfs(k + 1);
+        permutation_with_repetition.pop_back();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    cin >> n >> m;
+    dfs(1);
+    return 0;
+}
