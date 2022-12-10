25223d8812019ce9d5cc5cda9be74083ae9ae581
Dain Jung <dainjungdev@gmail.com>
2022-12-10 05:59:57 +0000

    solve 15650.cpp

---
+++ 15650.N과 M （2）/15650.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+bool visited[9];
+vector<int> combination;
+
+void dfs(int k) {
+    if (combination.size() == m) {
+        for (auto &num : combination) {
+            cout << num << ' ';
+        }
+        cout << '\n';
+    }
+
+    for (int i = k; i <= n; ++i) {
+        if (!visited[i]) {
+            visited[i] = true;
+            combination.push_back(i);
+            dfs(i + 1);
+            combination.pop_back();
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    dfs(1);
+    return 0;
+}
