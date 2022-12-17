e23fefdc08f5a2c5525cd023d3482d1f8e49744b
Dain Jung <dainjungdev@gmail.com>
2022-12-17 11:55:58 +0000

    solve 9251.cpp

---
+++ 09251.LCS/9251.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string a, b;
+    cin >> a >> b;
+
+    vector<vector<int>> lcs(a.length() + 1, vector<int>(b.length() + 1));
+
+    for (int i = 1; i <= a.length(); ++i) {
+        for (int j = 1; j <= b.length(); ++j) {
+            if (a[i - 1] == b[j - 1]) {
+                lcs[i][j] = lcs[i - 1][j - 1] + 1;
+            } else {
+                lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1]);
+            }
+        }
+    }
+
+    cout << lcs[a.length()][b.length()];
+    return 0;
+}
