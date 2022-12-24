9169ff84d8b9b7a5d7af32b583539017f3ce5544
Dain Jung <dainjungdev@gmail.com>
2022-12-24 20:38:40 +0100

    solve 10773.cpp

---
+++ 10773.제로/10773.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int k;
+    cin >> k;
+
+    stack<int> s;
+    int n;
+    for (int i = 0; i < k; ++i) {
+        cin >> n;
+        if (n == 0) {
+            s.pop();
+        } else {
+            s.push(n);
+        }
+    }
+
+    int sum = 0;
+    while (!s.empty()) {
+        sum += s.top();
+        s.pop();
+    }
+
+    cout << sum;
+    return 0;
+}
